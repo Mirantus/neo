7ed0c3fed2b5620995357cf40a24802ac953cbac
Mikhail Miropolskiy <the-ms@ya.ru>
2020-03-28 17:03:12 +0700

    404 страница

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -17,7 +17,8 @@ const Login = React.lazy(() => import(/* webpackChunkName: "item" */ "../pages/u
 const Profile = React.lazy(() => import(/* webpackChunkName: "item" */ "../pages/user/profile"));
 const Register = React.lazy(() => import(/* webpackChunkName: "item" */ "../pages/user/register"));
 
-const Home = React.lazy(() => import(/* webpackChunkName: "home" */ "../pages/Home"));
+const Home = React.lazy(() => import(/* webpackChunkName: "home" */ "../pages/home"));
+const NotFound = React.lazy(() => import(/* webpackChunkName: "notFound" */ "../pages/notFound"));
 
 // prepare store
 const history = createBrowserHistory();
@@ -29,14 +30,15 @@ const Application = () => (
             <Layout>
                 <React.Suspense fallback={<Loader />}>
                     <Switch>
-                        <Route path="/add" component={Add} />
-                        <Route path="/items/:id(\d+)" component={Item} />
+                        <Route exact={true} path="/add" component={Add} />
+                        <Route exact={true} path="/items/:id(\d+)" component={Item} />
 
-                        <Route path="/login" component={Login} />
-                        <Route path="/register" component={Register} />
-                        <PrivateRoute path="/profile" component={Profile} />
+                        <Route exact={true} path="/login" component={Login} />
+                        <Route exact={true} path="/register" component={Register} />
+                        <PrivateRoute exact={true} path="/profile" component={Profile} />
 
-                        <Route path="/" component={Home} />
+                        <Route exact={true} path="/" component={Home} />
+                        <Route path="*" component={NotFound} />
                     </Switch>
                 </React.Suspense>
             </Layout>
---
+++ src/pages/notFound.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const PageNotFound = () => (
+    <div className="container">
+        <h1 className="title">404</h1>
+        <p>Страница не найдена</p>
+    </div>
+);
+
+export default PageNotFound;
