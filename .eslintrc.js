de1d53af5399a7e1f6b4b3c5b888fb4d5602af58
Mikhail Miropolskiy <the-ms@ya.ru>
2020-05-22 07:52:40 +0700

    eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier"
+    "prettier",
   ],
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: "module",
   },
-  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  plugins: ["react", "react-hooks", "@typescript-eslint", "import"],
   rules: {
     "default-case": "error",
     "dot-notation": "error",
@@ -37,6 +37,7 @@ module.exports = {
     "prefer-const": "error",
     "prefer-destructuring": "error",
     "valid-jsdoc": "error",
+    "import/order": "error",
     "react/default-props-match-prop-types": "error",
     "react/jsx-boolean-value": "error",
     "react/jsx-curly-brace-presence": "error",
@@ -48,13 +49,13 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/explicit-function-return-type": "off"
+    "@typescript-eslint/explicit-function-return-type": "off",
   },
   settings: {
     react: {
       createClass: "createReactClass",
       pragma: "React",
-      version: "detect"
-    }
-  }
+      version: "detect",
+    },
+  },
 };
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ npm i react-router && npm i -D @types/react-router @types/history
 npm i redux react-redux redux-thunk && npm i -D @types/react-redux
 npm i -D redux-devtools-extension
 npm i -D @types/node
-npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-loader eslint-plugin-react eslint-plugin-react-hooks
+npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-loader eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-import
 npm i prettier && npm i -D lint-staged husky
 npm i -D webpack-dev-server react-hot-loader @hot-loader/react-dom
 npm i -D mini-css-extract-plugin css-loader less-loader less postcss-loader autoprefixer
